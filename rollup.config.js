15f5fa128fd6a4e39ba4c8843f369bbc757d761e
Andreas Wiedel <andreas.wiedel@smartservice.de>
2018-07-16 16:42:01 +0200

    added base structure

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+/* eslint-disable sort-keys */
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import {terser} from 'rollup-plugin-terser';
+import flow from 'rollup-plugin-flow';
+import pkg from './package.json';
+
+export default [
+    {
+        input: 'src/main.js',
+        output: {
+            name: 'simple-logger',
+            file: pkg.browser,
+            format: 'umd',
+        },
+        plugins: [
+            flow(),
+            resolve(),
+            commonjs(),
+            terser(),
+        ],
+    },
+    {
+        input: 'src/main.js',
+        external: ['ramda', 'lodash'],
+        output: [
+            {file: pkg.module, format: 'es'},
+        ],
+        plugins: [
+            flow(),
+        ],
+    },
+    {
+        input: 'src/main.js',
+        external: ['ramda', 'lodash'],
+        output: [
+            {file: pkg.main, format: 'cjs'},
+        ],
+        plugins: [
+            flow(),
+            terser(),
+        ],
+    },
+];
