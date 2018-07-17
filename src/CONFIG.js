a0e4f6d827ec50013d7d8e6fbd383949803eae46
Andreas Wiedel <andreas.wiedel@smartservice.de>
2018-07-17 15:35:15 +0200

    closed #7 made context padded

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -2,6 +2,7 @@ import LOG_LEVEL from './LOG_LEVEL';
 
 const CONFIG = {
     defaultDateFormat: 'HH:mm:ss.SSS',
+    defaultMaxContextLength: 20,
     defaultMinimumLogLevel: LOG_LEVEL.DEBUG,
     localStorageKeys: {
         dateFormat: 'simple-logger.dateFormat',
