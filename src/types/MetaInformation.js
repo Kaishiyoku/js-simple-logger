14936a1986f4160bb1a8cde8d47bfea19e79d7fe
Andreas Wiedel <andreas.wiedel@smartservice.de>
2018-07-19 13:41:47 +0200

    closed #16 fixed ESLint warnings

--- src/types/MetaInformation.js
+++ src/types/MetaInformation.js
@@ -1,9 +1,7 @@
 import LOG_LEVEL from '../data/LOG_LEVEL';
 
-type MetaInformation = {
+export type MetaInformation = {
     dateFormat: string,
     logLevel: LOG_LEVEL,
     context: string | null
 };
-
-export type MetaInformation;
