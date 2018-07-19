5c2602c0e15d956fba45e443a399f746529a92ad
Andreas Wiedel <andreas.wiedel@smartservice.de>
2018-07-19 12:55:12 +0200

    closed #13 added function to get info about the current global log level

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import {getLogger, setDateFormat, setMinimumLogLevel, setFormatter, getLogLevels} from './Logger';
+import {getLogger, setDateFormat, setMinimumLogLevel, getMinimumLogLevel, setFormatter, getLogLevels} from './Logger';
 import formatLogLevel from './helpers/formatLogLevel';
 
 export {
     getLogger,
     setDateFormat,
     setMinimumLogLevel,
+    getMinimumLogLevel,
     setFormatter,
     getLogLevels,
     formatLogLevel,
