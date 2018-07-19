47c6d2f5cebe05c3c5c16d333fc9e3b50f0186f3
Andreas Wiedel <andreas.wiedel@smartservice.de>
2018-07-19 11:26:19 +0200

    #9 code refactorings

--- src/helpers/log.js
+++ src/helpers/log.js
@@ -2,10 +2,11 @@ import withMinimumLogLevel from './withMinimumLogLevel';
 import logFormatter from './logFormatter';
 
 const log = (handler) => (metaInformation) => (...messages) => {
+    const withMinimumLogLevelAndMeta = withMinimumLogLevel(metaInformation);
     const formatterFn = logFormatter(metaInformation);
     const handlerFn = handler(...formatterFn(messages));
 
-    return withMinimumLogLevel(metaInformation)(handlerFn);
+    return withMinimumLogLevelAndMeta(handlerFn);
 };
 
 export default log;
